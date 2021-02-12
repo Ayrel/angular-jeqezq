84fc2fea11864697c8bb2cc5aafa77e2674599c2
Ayrel <rossillol@gmail.com>
2021-02-12 15:22:33 +0100

    Initial commit

---
+++ src/app/hello.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: '[svgtext]',
+  template: `<svg:text x="0" y="65">test text</svg:text>`,
+  styles: []
+})
+export class HelloComponent  {
+
+}
